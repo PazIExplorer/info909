ba2b46b496e7523b6e6a7689da65b9ce76cbfcdf
unknown <firedestroye.eligriv@gmail.com>
2022-01-12 09:03:08 +0100

    ajout fonction

--- app/test/unit.test.js
+++ app/test/unit.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { sub, mul, add } = require("../functions");
+const { sub, mul, add,pow } = require("../functions");
 describe('Tests unitaire', () => {
     it('should return -3', () => {
         assert.equal(sub(2, 5), -3);
@@ -9,5 +9,8 @@ describe('Tests unitaire', () => {
     });
     it('should return 12', () => {
         assert.equal(mul(6, 2), 12);
+    });
+    it('should return 9',()=> {
+        assert.equal(pow(3,2), 9);
     });
 });
